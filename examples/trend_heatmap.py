b72d10c624b87bbd9b5616ee838f50a2b4cf9f5f
psazama <patricia@patriciasazama.me>
2025-12-15 01:31:24 +0000

    cleaning up unnecessary files

--- examples/trend_heatmap.py
+++
@@ -1,6 +0,0 @@
-"""Convenience launcher for generating a water-trend heatmap visualization."""
-
-from swmaps.pipeline.trend import trend_heatmap
-
-if __name__ == "__main__":
-    trend_heatmap()
--- swmaps/resources/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Resource definitions and lookup tables used by the pipeline."""
-
-from .cdl_classes import CDL_CLASSES
-
-__all__ = ["CDL_CLASSES"]
